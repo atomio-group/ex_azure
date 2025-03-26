49720ed5d63b222055785c4dfc5f94b6647cfbd6
Kurt Vincent Bautista <kurt.bautista@lawadvisor.com>
2025-03-26 11:18:13 +0800

    Override jsx version

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule ExAzure.Mixfile do
   defp deps do
     [
       {:erlazure, github: "dkataskin/erlazure", manager: :rebar3},
+      {:jsx, "~> 3.0", override: true},
       {:ex_doc, "~> 0.11.5", only: [:dev, :test]},
       {:earmark, "~> 0.2.1", only: [:dev, :test]}
     ]
